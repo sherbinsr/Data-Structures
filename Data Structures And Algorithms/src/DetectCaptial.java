8331797d678e38ba5f6d148eac32edcb091daaed
sherbinsr <sherbinsyles31@gmail.com>
2024-01-17 21:44:19 +0530

    DSA

---
+++ Data Structures And Algorithms/src/DetectCaptial.java
@@ -0,0 +1,35 @@
+public class DetectCaptial {
+
+    public static void main(String[] args) {
+
+        DetectCaptial dc = new DetectCaptial();
+        System.out.println(dc.detectCapitalUse("Leetcode"));
+
+    }
+
+    public boolean detectCapitalUse(String word) {
+
+
+
+        String upperCase=word.toUpperCase();
+        String lowerCase=word.toLowerCase();
+        
+
+        int size = word.length();
+
+
+        if(word==upperCase || word.length()==1 || size<2 || word==lowerCase)
+        {
+            return  true;
+        }
+        else
+        {
+            return  false;
+        }
+
+
+
+
+    }
+
+}
