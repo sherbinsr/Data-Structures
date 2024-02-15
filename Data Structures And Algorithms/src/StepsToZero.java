3e32b0a2714721e9a0f03ae1c608cf731c3ba8ac
sherbinsr <sherbinsyles31@gmail.com>
2024-02-15 21:52:05 +0530

    DSA

---
+++ Data Structures And Algorithms/src/StepsToZero.java
@@ -0,0 +1,31 @@
+public class StepsToZero {
+
+
+    public static void main(String[] args) {
+
+        StepsToZero stepsToZero = new StepsToZero();
+        System.out.println(stepsToZero.numberOfSteps(14));
+    }
+    public int numberOfSteps(int num) {
+
+        int count=0;
+
+
+        while (num!=0) {
+
+            count++;
+
+            if(num%2==0)
+            {
+                num=num/2;
+            }
+            else
+            {
+                num=num-1;
+            }
+        }
+
+
+        return count;
+    }
+}
