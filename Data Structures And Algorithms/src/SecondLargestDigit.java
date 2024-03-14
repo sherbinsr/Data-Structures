359509f4929ed42c0ce7a08df8ae3290287cf4f0
sherbinsr <sherbinsyles31@gmail.com>
2024-03-14 22:14:59 +0530

    DSA

---
+++ Data Structures And Algorithms/src/SecondLargestDigit.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SecondLargestDigit
+{
+    public static void main(String[] args) {
+
+        SecondLargestDigit largestDigit = new SecondLargestDigit();
+        System.out.println(largestDigit.secondHighest("dfa12321afd"));
+    }
+    public int secondHighest(String s)
+    {
+            HashSet<Integer>hashSet = new HashSet<>();
+
+            for(char c:s.toCharArray())
+            {
+                if(Character.isDigit(c))
+                {
+                    hashSet.add(c-'0');
+                }
+            }
+            int[] arr=new int[hashSet.size()];
+
+            int j=0;
+            for(int setValue:hashSet)
+            {
+                arr[j]=setValue;
+                j++;
+            }
+            Arrays.sort(arr);
+            if(arr.length<2)
+            {
+                return -1;
+            }
+            else
+            {
+                return arr[arr.length-2];
+            }
+    }
+}
