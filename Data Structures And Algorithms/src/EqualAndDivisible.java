f9043b6889f587e3a9aafc20344907a574d5f349
sherbinsr <sherbinsyles31@gmail.com>
2024-03-25 12:35:14 +0530

    DSA

--- Data Structures And Algorithms/src/EqualAndDivisible.java
+++ Data Structures And Algorithms/src/EqualAndDivisible.java
@@ -2,15 +2,24 @@ public class EqualAndDivisible
 {
     public static void main(String[] args) {
 
-        int [] nums ={3,1,2,2,2,1,3};
+        int [] nums ={5,5,9,2,5,5,9,2,2,5,5,6,2,2,5,2,5,4,3};
         EqualAndDivisible equalAndDivisible = new EqualAndDivisible();
-        System.out.println(equalAndDivisible.countPairs(nums,2));
+        System.out.println(equalAndDivisible.countPairs(nums,7));
     }
     public int countPairs(int[] nums, int k) {
 
         int count=0;
 
-        for(int i=0; i< nums.length)
+        for(int i=0; i< nums.length; i++)
+        {
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i]==nums[j]  && (i*j)%k==0)
+                {
+                    count++;
+                }
+            }
+        }
         return count;
 
     }
