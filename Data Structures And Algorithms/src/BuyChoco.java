460ed521ce10b7b57b852529e5019a1a8d171e90
sherbinsr <sherbinsyles31@gmail.com>
2024-04-17 21:23:19 +0530

    DSA

--- Data Structures And Algorithms/src/BuyChoco.java
+++ Data Structures And Algorithms/src/BuyChoco.java
@@ -1,51 +1,26 @@
+import java.util.Arrays;
+
 public class BuyChoco {
 
     public static void main(String[] args) {
 
-        int prices[]={1,2,2};
+        int prices[]={98,54,6,34,66,63,52,39};
         BuyChoco buyChoco = new BuyChoco();
-       int res = buyChoco.buyChoco(prices,3);
+       int res = buyChoco.buyChoco(prices,62);
         System.out.println(res);
 
     }
     public int buyChoco(int[] prices, int money)
     {
-            int lowest=prices[0];
-            int secondlowest=prices[0];
-        for (int i=0; i< prices.length-1; i++)
+
+        Arrays.sort(prices);
+
+        int buy=prices[0]+prices[1];
+
+        if(buy<=money)
         {
-            if(lowest>prices[i+1])
-            {
-
-                lowest=prices[i];
-            }
-            if(secondlowest>prices[i+1])
-            {
-                    if(prices[i]==lowest)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        secondlowest = prices[i];
-                    }
-            }
-            if(prices[0]==money)
-            {
-                return money;
-            }
-            else if (prices[i]<=money)
-            {
-
-                money =money-prices[i];
-            } else if (prices[i]<=money)
-            {
-                int calc = lowest+secondlowest;
-                money =money-prices[i];
-            }
-
+            return money-buy;
         }
-
 
 
         return money;
