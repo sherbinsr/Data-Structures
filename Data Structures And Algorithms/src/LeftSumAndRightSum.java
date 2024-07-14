b76bb753c48b8889d48378ddbd050770fc7e95b3
sherbinsr <sherbinsyles31@gmail.com>
2024-07-14 22:09:25 +0530

    DSA

---
+++ Data Structures And Algorithms/src/LeftSumAndRightSum.java
@@ -0,0 +1,73 @@
+public class LeftSumAndRightSum
+{
+    public static void main(String[] args) {
+
+
+        int [] nums ={10,4,8,3};
+        LeftSumAndRightSum leftSumAndRightSum = new LeftSumAndRightSum();
+        int res [] = leftSumAndRightSum.leftRightDifference(nums);
+
+        for (int r:res)
+        {
+            System.out.println(r);
+        }
+
+
+    }
+    public int[] leftRightDifference(int[] nums) {
+
+        int leftSum[] = leftSum(nums);
+
+        int rightSum[] = rightSum(nums);
+
+        int resSum[] = new  int[nums.length];
+
+
+
+        for(int i=0; i< nums.length; i++)
+        {
+            int sum=leftSum[i]-rightSum[i];
+
+            resSum[i]=Math.abs(sum);
+        }
+
+
+
+        return resSum;
+
+    }
+    public  int[] rightSum(int[]nums)
+    {
+        int rightSum[] = new int[nums.length];
+        for (int i=0; i< nums.length; i++)
+        {
+            int sum =0;
+            for (int j=i+1; j< nums.length; j++)
+            {
+                sum+=nums[j];
+            }
+           rightSum[i]=sum;
+        }
+        return rightSum;
+    }
+    public int[] leftSum(int[] nums)
+    {
+        int leftSum[] = new int[nums.length];
+
+        leftSum[0]=0;
+
+        int sum=0;
+        int j=1;
+        for(int i=0; i< nums.length-1; i++)
+        {
+            sum+=nums[i];
+            leftSum[j]=sum;
+            if(j< nums.length) {
+                j = j + 1;
+            }
+
+        }
+        return leftSum;
+
+    }
+}
