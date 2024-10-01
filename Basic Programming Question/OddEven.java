e50700095a594977b8e35e7672bd1a9a5dc7263f
sherbinsr <sherbinsyles31@gmail.com>
2024-10-01 13:06:10 +0530

    odd even

---
+++ Basic Programming Question/OddEven.java
@@ -0,0 +1,18 @@
+public class OddEven
+{
+    public static void main(String[] args) {
+
+
+        int num =4;
+        if(num%2!=0)
+        {
+            System.out.println("odd");
+        }
+        else
+        {
+            System.out.println("even");
+        }
+
+
+    }
+}
