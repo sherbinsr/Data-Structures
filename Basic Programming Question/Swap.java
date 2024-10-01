5398f4ddb278d8fe29d0978983862fdc32b6168b
sherbinsr <sherbinsyles31@gmail.com>
2024-10-01 12:39:04 +0530

    Swap

---
+++ Basic Programming Question/Swap.java
@@ -0,0 +1,17 @@
+public class Swap
+{
+    public static void main(String[] args) {
+
+        int a =10;
+
+        int b =20;
+
+        b =b+a;
+        a= b-a;
+        b= b-a;
+
+        System.out.println("a="+a +","+"b="+b);
+
+
+    }
+}
